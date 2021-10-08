b25cb1e910f7ba78a1635e2f364c28ed470c2ec7
Elizaveta Anisimova <el1za.anisimova@yandex.ru>
2021-10-08 18:22:04 +0300

    Change permissions

--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -3,6 +3,6 @@ from rest_framework import permissions
 
 class AuthenticatedOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method == 'GET' and request.user.is_authenticated:
+        if request.method == 'GET' or request.user.is_authenticated:
             return True
         return False
