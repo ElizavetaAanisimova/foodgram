8f97851b99d2fd1751628089a4d1398722b84d02
Elizaveta Anisimova <el1za.anisimova@yandex.ru>
2021-10-09 12:48:09 +0300

    remove smallintegerfieldvalidator

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -137,7 +137,8 @@ class IngredientInRecipe(models.Model):
         verbose_name='Рецепт'
     )
     amount = models.SmallIntegerField(
-        validators=[MinValueValidator(1, 'Количество не может быть меньше 1')],
+        # validators=[MinValueValidator(1,
+        #  'Количество не может быть меньше 1')],
         default=1,
         verbose_name='Количество ингредиента'
     )
